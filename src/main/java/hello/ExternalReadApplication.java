d86dfc58a1a524a413e1b44843cba48219c318ac
minu <made_power@naver.com>
2023-06-16 13:10:18 +0900

    @ConfigurationProperties 검증
    spring validator를 사용하여 설정정보를 쉽게 검증할 수 있다.

--- src/main/java/hello/ExternalReadApplication.java
+++ src/main/java/hello/ExternalReadApplication.java
@@ -1,14 +1,11 @@
 package hello;
 
-import hello.config.MyDataSourceConfigV1;
-import hello.config.MyDataSourceConfigV2;
-import hello.config.MyDataSourceEnvConfig;
-import hello.config.MyDataSourceValueConfig;
+import hello.config.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
-@Import(MyDataSourceConfigV2.class)
+@Import(MyDataSourceConfigV3.class)
 @SpringBootApplication(scanBasePackages = "hello.datasource")
 public class ExternalReadApplication {
 
